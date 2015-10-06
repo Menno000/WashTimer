b00e65e3ff47e4d703b7036da7d0688aca27d115
Jonathan L Clark <jonathan84clark@yahoo.com>
2015-10-06 06:55:48 -0700

    Adding CCS project files for the wash timer. The timer is fully operational.

---
+++ WashTimer/WashTimer/Debug/main.pp
@@ -0,0 +1,15 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/Programming/CCS6/ccsv6/ccs_base/msp430/include/msp430.h
+main.obj: C:/Programming/CCS6/ccsv6/ccs_base/msp430/include/msp430g2452.h
+main.obj: C:/Programming/CCS6/ccsv6/ccs_base/msp430/include/in430.h
+main.obj: C:/Programming/CCS6/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics.h
+main.obj: C:/Programming/CCS6/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics_legacy_undefs.h
+
+../main.c: 
+C:/Programming/CCS6/ccsv6/ccs_base/msp430/include/msp430.h: 
+C:/Programming/CCS6/ccsv6/ccs_base/msp430/include/msp430g2452.h: 
+C:/Programming/CCS6/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/Programming/CCS6/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics.h: 
+C:/Programming/CCS6/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics_legacy_undefs.h: 
